14b67224386d664ceff879017ea36b54cf279658
Nicholas Konar <nkonar@protonmail.com>
2025-03-29 14:18:58 -0400

    Detect target impact

--- index.js
+++ index.js
@@ -1,56 +1,96 @@
 let projectile, target;
-const dt = 0.1; 
-const g = 9.81; 
+const dt = 0.1;
+const g = 9.81;
 const k = 0.05;
 
 function setup() {
   createCanvas(1500, 800);
-  let angle = radians(40);
-  let speed = 150;
+  let angle = radians(30);
+  let speed = 175;
   let initialVelocity = createVector(speed * cos(angle), speed * sin(angle));
-  projectile = new Projectile(0, 0, initialVelocity);
-  target = new Target(width * 0.9, 100, 50);
+  projectile = new Projectile(0, 0, initialVelocity, 5);
+  target = new Target(width * 0.9, 100, 250);
 }
 
 function draw() {
   background(220);
   translate(0, height);
   scale(1, -1);
-
+
   target.display();
 
   projectile.update();
   projectile.display();
 
+  if (projectile.pos.x >= target.pos.lower.x) {
+    const impact = target.impactedBy(projectile);
+    if (impact) {
+      console.log(`Impact x: ${impact.x} y: ${impact.y}`);
+      push();
+      stroke("red");
+      strokeWeight(5);
+      point(impact.x, impact.y);
+      pop();
+      noLoop();
+    } else {
+      console.log("Target missed.");
+    }
+  }
+
   if (
+    projectile.pos.x < 0 ||
     projectile.pos.x > width ||
-    projectile.pos.y > height ||
-    projectile.pos.x < 0
+    projectile.pos.y < 0 ||
+    projectile.pos.y > height
   ) {
     noLoop();
   }
 }
 
+class Target {
+  constructor(x, y, height) {
+    this.pos = {
+      upper: createVector(x, y + height),
+      lower: createVector(x, y),
+    };
+  }
+
   display() {
     push();
-    stroke(255, 0, 0);
-    strokeWeight(10);
-    line(this.pos.x, this.pos.y, this.pos.x, this.pos.y + this.height);
+    stroke("grey");
+    strokeWeight(1);
+    line(
+      this.pos.lower.x,
+      this.pos.lower.y,
+      this.pos.upper.x,
+      this.pos.upper.y,
+    );
     pop();
+  }
+
+  impactedBy(projectile) {
+    const a = projectile.pos;
+    const b = projectile.prev;
+    const c = this.pos.lower;
+    const d = this.pos.upper;
+    return intersects2D(a, b, c, d);
   }
 }
 
 class Projectile {
-  constructor(x, y, velocity) {
+  constructor(x, y, velocity, size) {
     this.pos = createVector(x, y);
     this.vel = velocity.copy();
+    this.prev = undefined;
+    this.size = size;
   }
 
   update() {
-    let gravity = createVector(0, -g);
-    let drag = this.vel.copy().mult(-k);
+    this.prev = this.pos.copy();
+    const gravity = createVector(0, -g);
+    const drag = this.vel.copy().mult(-k);
 
-    let acceleration = p5.Vector.add(gravity, drag);
+    const acceleration = p5.Vector.add(gravity, drag);
 
     // Euler integration:
     this.vel.add(p5.Vector.mult(acceleration, dt));
@@ -58,11 +98,7 @@ class Projectile {
   }
 
   display() {
-    circle(this.pos.x, this.pos.y, 15);
-    //let vec = this.vel.copy();
-    //let start = this.pos.copy().add(vec);
-    //let end = this.pos.copy().sub(vec);
-    //line(start.x, start.y, end.x, end.y);
+    circle(this.pos.x, this.pos.y, this.size);
   }
 }
 
@@ -88,3 +124,4 @@ function intersects2D(a, b, c, d) {
     return false;
   }
 }
+
