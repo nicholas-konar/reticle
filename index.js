ef3c7d619aaf33b8870fbc09c37192bfd5d19774
Nicholas Konar <nkonar@protonmail.com>
2025-03-29 13:18:18 -0400

    Line intersection function (2D only)

--- index.js
+++ index.js
@@ -66,3 +66,25 @@ class Projectile {
   }
 }
 
+function intersects2D(a, b, c, d) {
+  const denominator = (b.x - a.x) * (d.y - c.y) - (b.y - a.y) * (d.x - c.x);
+
+  if (denominator === 0) return false;
+
+  const numeratorT = (c.x - a.x) * (d.y - c.y) - (c.y - a.y) * (d.x - c.x);
+  const numeratorU = (c.x - a.x) * (b.y - a.y) - (c.y - a.y) * (b.x - a.x);
+
+  const t = numeratorT / denominator;
+  const u = numeratorU / denominator;
+
+  const validT = 0 <= t && t <= 1;
+  const validU = 0 <= u && u <= 1;
+
+  if (validT && validU) {
+    const x = a.x + t * (b.x - a.x);
+    const y = a.y + t * (b.y - a.y);
+    return createVector(x, y);
+  } else {
+    return false;
+  }
+}
