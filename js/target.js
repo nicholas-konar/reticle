7eda628d6b89073acf4ce701ffe17e4ed0488636
Nicholas Konar <nkonar@protonmail.com>
2025-04-02 15:54:59 -0400

    Refactor target coord system

--- js/target.js
+++ js/target.js
@@ -2,31 +2,21 @@ export class Target {
   constructor(pos, w, h, l) {
     this.pos = pos;
     this.dim = { w, h, l };
-    this.centerMass = pos.copy().add(0, h / 2, 0);
     this.borders = {
-      topY: pos.y + h,
-      bottomY: pos.y,
+      topY: pos.y + h / 2,
+      bottomY: pos.y - h / 2,
       leftZ: -(w / 2),
       rightZ: w / 2,
     };
   }
 
-  // pos represents the middle of the bottom edge of the target face.
-  // p5.js puts the center of the shape at the origin, so we
-  // translate & rotate to treat the center of the bottom edge as pos
-  display(impact) {
+  display() {
     push();
     stroke("black");
     strokeWeight(1);
-    translate(
-      this.pos.x + this.dim.l / 2,
-      -(this.pos.y + this.dim.h / 2),
-      this.pos.z,
-    );
+    translate(this.pos.x + this.dim.l / 2, -this.pos.y, this.pos.z);
     rotateY(PI / 2);
     box(this.dim.w, this.dim.h, this.dim.l);
-    //stroke("red");
-    //if (impact) cylinder(10, 5);
     pop();
   }
 
@@ -48,7 +38,7 @@ export class Target {
     const checkZ = p.z <= rightZ && leftZ <= p.z;
 
     const impact = checkX && checkY && checkZ;
-    const delta = p5.Vector.sub(p, this.centerMass);
+    const delta = p5.Vector.sub(this.pos, p);
 
     return { impact, delta };
   }
