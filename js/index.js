83d5c3c8f294843abf995252cb4cf3fef8a7b5fb
Nicholas Konar <nkonar@protonmail.com>
2025-04-03 19:04:27 -0400

    Code cleanup

--- js/index.js
+++ js/index.js
@@ -1,20 +1,24 @@
-import { Projectile } from "/js/projectile.js";
-import { Target } from "/js/target.js";
+/// <reference types="p5" />
+import { Projectile } from "./projectile.js";
+import { Target } from "./target.js";
 
 let projectile, target, cam, ground;
-const distBehindTarget = 100;
+const distBehindTarget = 1000;
 
 window.setup = () => {
   createCanvas(1000, 800, WEBGL);
+  // units in cm
   const shooterPosition = createVector(0, 0, 0);
-  const cameraPosition = createVector(-100, 50, 50);
-  const targetPosition = createVector(1000, 100, 0);
+  const cameraPosition = createVector(0, 0, 50);
+  // 2000cm ~ 22 yards
+  const targetPosition = createVector(20000, 0, 0);
+  const targetDimension = createVector(50, 100, 2);
 
   setupCamera(cameraPosition);
-  setupTarget(targetPosition);
+  setupTarget(targetPosition, targetDimension);
   setupProjectile(shooterPosition);
 
-  ground = min(shooterPosition.y, target.borders.bottomY) - 5;
+  ground = min(shooterPosition.y, target.borders.bottomY) - 100;
   cam.lookAt(target.pos.x, -target.pos.y, target.pos.z);
 };
 
@@ -22,20 +26,24 @@ window.draw = () => {
   background(220);
 
   target.display();
-  projectile.update();
   projectile.display();
   setupGroundPlane();
+
+  projectile.update();
+  projectile.displayTrail();
 
   if (projectile.pos.x >= target.pos.x) {
     const px = projectile.at(target.pos.x);
     const { impact, delta } = target.impactedBy(px);
 
-    if (impact) target.displayImpact(px), noLoop();
-
     const report = generateReport(impact, delta);
     console.log(report);
 
-    projectile.displayTrail();
+    if (impact) {
+      target.displayImpact(px);
+      console.log(projectile.trail);
+      noLoop();
+    }
   }
 
   const outOfFrame =
@@ -49,20 +57,24 @@ window.draw = () => {
   }
 };
 
+/** @param {import('p5').Vector} pos - 3D vector */
 function setupCamera(pos) {
   cam = createCamera();
   cam.setPosition(pos.x, -pos.y, pos.z);
   setCamera(cam);
 }
 
-function setupTarget(pos) {
-  target = new Target(pos, 50, 100, 2);
+/** @param {import('p5').Vector} pos - 3D vector
+ * @param {import('p5').Vector} dim - 3D vector */
+function setupTarget(pos, dim) {
+  target = new Target(pos, dim);
 }
 
+/** @param {import('p5').Vector} pos - 3D vector */
 function setupProjectile(pos) {
-  const angle = radians(6);
+  const angle = radians(0);
   const windage = radians(0);
-  const speed = 1000;
+  const speed = 3000;
   const initialVelocity = createVector(
     speed * cos(angle),
     speed * sin(angle),
@@ -97,7 +109,7 @@ function generateReport(impact, delta) {
     report.push(`${abs(delta.z)} units left`);
   }
 
-  if (report.length > 1) report.splice(2, 0, "and");
+  if (report.length > 2) report.splice(2, 0, "and");
   report.push("from center mass.");
 
   return report.join(" ");
